1f4e7aa759ea34f2957de81ac0aa498cf479f911
laudanum <laudanum@laudans.de>
2023-01-18 21:03:32 +0100

    additional changes

--- frontend/components/adventure_builder.vue
+++ frontend/components/adventure_builder.vue
@@ -1,23 +1,35 @@
 <template>
   <div>
-    <h1>Adventure Builder</h1>
-    
-    <input type="text" v-model="adventureName"/>
-    <button @click="createAdventure()">Create Adventure</button>
+    <form>
+      <label for="adventure-title">Adventure Title:</label>
+      <input type="text" id="adventure-title" v-model="adventureTitle" placeholder="Enter Adventure Title">
+      <br><br>
+      <label for="adventure-setting">Adventure Setting:</label>
+      <input type="text" id="adventure-setting" v-model="adventureSetting" placeholder="Enter Adventure Setting">
+      <br><br>
+      <label for="adventure-plot">Adventure Plot:</label>
+      <textarea id="adventure-plot" v-model="adventurePlot" placeholder="Enter Adventure Plot"></textarea>
+      <br><br>
+      <button v-on:click="generateAdventure()">Generate Adventure</button>
+    </form>
   </div>
 </template>
 
 <script>
-  export default {
-    data() {
-      return {
-        adventureName: ''
-      }
-    },
-    methods: {
-      createAdventure() {
-        // Send AJAX request to backend with adventure name
-      }
+export default {
+  data() {
+    return {
+      adventureTitle: '',
+      adventureSetting: '',
+      adventurePlot: '',
+    }
+  },
+  methods: {
+    generateAdventure() {
+      // Use GPT-3 to generate text based on the input provided by the user
+      // You can use axios or any other http library to call the GPT-3 API
+      // and generate the text and then display it on the page
     }
   }
+}
 </script>
--- frontend/components/character_generator.vue
+++ frontend/components/character_generator.vue
@@ -1,23 +1,35 @@
 <template>
-  <div>
-    <h1>Community Support</h1>
-    
-    <input type="text" v-model="communityName"/>
-    <button @click="createCommunity()">Create Community</button>
-  </div>
+    <div>
+        <form>
+            <label for="character-name">Character Name:</label>
+            <input type="text" id="character-name" v-model="characterName" placeholder="Enter Character Name">
+            <br><br>
+            <label for="character-race">Character Race:</label>
+            <input type="text" id="character-race" v-model="characterRace" placeholder="Enter Character Race">
+            <br><br>
+            <label for="character-class">Character Class:</label>
+            <input type="text" id="character-class" v-model="characterClass" placeholder="Enter Character Class">
+            <br><br>
+            <button v-on:click="generateCharacter()">Generate Character</button>
+        </form>
+    </div>
 </template>
 
 <script>
-  export default {
+export default {
     data() {
-      return {
-        communityName: ''
-      }
+        return {
+            characterName: '',
+            characterRace: '',
+            characterClass: '',
+        }
     },
     methods: {
-      createCommunity() {
-        // Send AJAX request to backend with community name
-      }
+        generateCharacter() {
+            // Use GPT-3 to generate text based on the input provided by the user
+            // You can use axios or any other http library to call the GPT-3 API
+            // and generate the text and then display it on the page
+        }
     }
-  }
+}
 </script>
