8bde3cf78b6c0a8588e0176a36df5ca8dbe5d2d5
laudanum <laudanum@laudans.de>
2023-02-06 08:24:51 +0100

    update npc frontend and create backend stump

--- backend/app.py
+++ backend/app.py
@@ -5,5 +5,13 @@
 from main import app
 
 
+@app.route("/generate_npc", methods=["POST"])
+def generate_npc():
+    """Create a new NPC for an Adventure using GPT-3 and return the result"""
+    print(request.json)
+    if request.json:
+
+
+
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000, debug=True)
