a7511dd1d0239267da567ece0fe495175bb51982
Yazid <yazeedkurdi@hotmail.com>
2023-02-14 20:33:09 +0300

    code refactor - card components

---
+++ frontend/src/components/Cards/AdventureView Card.vue
@@ -0,0 +1,33 @@
+<template>
+    <div class="card">
+      <div class="card-body">
+        <h4 class="card-title">{{ title }}</h4>
+        <p class="card-text"><slot></slot></p>
+      </div>
+    </div>
+    <br>
+  </template>
+  
+  <script>
+  export default {
+    name: 'MyCard',
+    props: {
+      title: {
+        type: String,
+        required: true
+      }
+    }
+  }
+  </script>
+  
+
+<style scoped>
+.card {
+    box-shadow: 0 4px 8px 0 rgba(0, 0, 0, 0.2);
+    transition: 0.3s;
+}
+
+.card:hover {
+    box-shadow: 0 8px 16px 0 rgba(0, 0, 0, 0.2);
+}
+</style>
