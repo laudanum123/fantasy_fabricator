3e3857027b987d3644fb01e7375c39b1755039af
Yazid <yazeedkurdi@hotmail.com>
2023-02-19 21:36:31 +0300

    Notification implementation using Toast

--- frontend/src/components/adventure_manager.vue
+++ frontend/src/components/adventure_manager.vue
@@ -1,6 +1,18 @@
 <template>
   <div class="container my-5">
     <h2 class="text-center mb-5 heading">Stored Adventures</h2>
+    <!-- Notification template -->
+    <template v-if="show">
+      <div class="container my-4">
+        <div class="toast-container position-absolute" :class="position">
+          <div class="toast" :class="show">
+            <ToastHeader :toast="toast" @hide="hideToast" />
+            <ToastBody :message="toast.message" />
+          </div>
+        </div>
+      </div>
+    </template>
+    <div id="message-container"></div>
     <div>
       <vue-good-table v-on:selected-rows-change="selectionChanged" :columns="columns" :rows="adventures"
         :pagination-options="{ enabled: true, mode: 'records', perPage: 10 }" :search-options="{ enabled: true }"
@@ -28,15 +40,21 @@
 }
 </style>
 <script>
+import ToastHeader from '@/components/Notifications/ToastHeader.vue';
+import ToastBody from '@/components/Notifications/ToastBody.vue';
+import moment from 'moment';
 import router from '@/router';
 import axios from 'axios'
 const backendURL = 'http://localhost:5000';
 
 export default {
   components: {
+    ToastHeader,
+    ToastBody
   },
   data() {
     return {
+      showToast: '',
       selection: [],
       columns: [
         {
@@ -71,40 +89,52 @@ export default {
         is: 'fa-sort',
         up: 'fa-sort-up',
         down: 'fa-sort-down'
-      }
+      }, toast: {
+        icon: '',
+        title: '',
+        time: '',
+        message: '',
+        bgColor: '',
+      },
+      show: '',
+      position: '',
+      timeout: null
     }
   },
   methods: {
-    getDb(){
+    getDb() {
       const path = `${backendURL}/get_adventures_from_db`;
-    axios.get(path)
-      .then(response => {
-        this.adventures = response.data.map(adventure => {
-          return {
-            'AdventureID': adventure.id,
-            'AdventureTitle': adventure.AdventureTitle,
-            'AdventureHook': adventure.AdventureHook,
-            'AdventureNPCs': adventure.AdventureNPCs,
-            'AdventurePlot': adventure.AdventurePlot,
-          }
-        });
-      })
-      .catch(error => {
-        console.log(error)
-      })
+      axios.get(path)
+        .then(response => {
+          this.adventures = response.data.map(adventure => {
+            return {
+              'AdventureID': adventure.id,
+              'AdventureTitle': adventure.AdventureTitle,
+              'AdventureHook': adventure.AdventureHook,
+              'AdventureNPCs': adventure.AdventureNPCs,
+              'AdventurePlot': adventure.AdventurePlot,
+              'EntitiesExtracted': adventure.EntitiesExtracted
+            }
+          });
+        })
+        .catch(error => {
+          console.log(error)
+        })
     },
     onView(params) {
       router.push({ path: '/AdventureView', query: { id: params['AdventureID'] } })
     },
-    onExtract(row_id){
-      const path = `${backendURL}/extract_entities/${row_id.AdventureID}`;
-    axios.post(path)
-      .then(response => {
-        console.log(response.data)
-      })
-      .catch(error => {
-        console.log(error)
-      })
+    onExtract(row_id) {
+      if (!row_id.EntitiesExtracted) {
+        this.successToast()
+        const path = `${backendURL}/extract_entities/${row_id.AdventureID}`;
+        axios.post(path)
+          .catch(error => {
+            console.log(error)
+          })
+        } else {
+          this.dangerToast()
+        }
     },
     onDelete() {
       const ids = this.selection.selectedRows.map(row => row['AdventureID']);
@@ -119,10 +149,47 @@ export default {
 
     selectionChanged(selectedRows) {
       this.selection = selectedRows
-    }
+    },
+    successToast() {
+      this.show = 'show';
+      this.position = 'top-0 end-0';
+      this.toast = {
+        icon: 'fa-solid fa-check me-2',
+        time: moment().fromNow(),
+        title: 'success',
+        bgColor: 'bg-success',
+        message: 'Extraction successful, click View for results!'
+      }
+    },
+    dangerToast() {
+      this.show = 'show';
+      this.position = 'top-0 end-0';
+      this.toast = {
+        icon: 'fa-solid fa-bolt me-2',
+        time: moment().fromNow(),
+        title: 'Error',
+        bgColor: 'bg-danger',
+        message: 'Adventure already extracted!'
+      }
+    },
+    hideToast() {
+      this.show = '';
+      this.position = '';
+    },
   },
-mounted() {
+  mounted() {
     this.getDb();
+  },
+  watch: {
+    show() {
+      if (this.timeout) {
+        clearTimeout(this.timeout);
+      }
+      this.timeout = setTimeout(() => {
+        this.show = '';
+        this.position = '';
+      }, 3000);
+    }
   }
 }
 </script>
