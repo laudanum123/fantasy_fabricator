57da5f0bc672d80b99b01afaf885a5f07c00216b
Yazid <yazeedkurdi@hotmail.com>
2023-02-27 21:53:23 +0300

    code refactor

--- backend/main/__init__.py
+++ backend/main/__init__.py
@@ -4,15 +4,10 @@ from flask_cors import CORS
 from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
-# app.config.from_object(__name__)
 app.config["SECRET_KEY"] = "5791628bb0b13ce0c676dfde280ba245"
 app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///../fantasy_fabricator.db"
-# app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///fantasy_fabricator.db'
 db = SQLAlchemy(app)
-# configure the SQLite database, relative to the app instance folder
 
-# app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///project.db"
-# initialize the app with the extension
 
 CORS(app, resources={r"/*": {"origins": "*"}})
 from main.routes.routes import routes
